16c502c834d1a99d2221ff346145c536cb8f54fc
Matei David <matei@buoyant.io>
2022-01-17 18:11:44 +0000

    Change LSP to underline
    Signed-off-by: Matei David <matei@buoyant.io>

--- nvim/lua/lang-conf.lua
+++ nvim/lua/lang-conf.lua
@@ -47,7 +47,7 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
     -- Enable underline, use default values
     underline = true,
     -- Enable virtual text, override spacing to 4
-    virtual_text = true,
+    virtual_text = false,
     signs = {
       enable = true,
       priority = 20
@@ -82,32 +82,9 @@ lspconfig.rust_analyzer.setup {
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
-
-require'lspconfig'.sumneko_lua.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = runtime_path,
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
+lspconfig.sumneko_lua.setup{
+  on_attach = on_attach
 }
-
 --require('rust-tools').setup({})
 
 require'nvim-treesitter.configs'.setup {
