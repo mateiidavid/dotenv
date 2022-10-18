cd3ed2dd27d330f017d2c12c1164c36971c6ef70
Matei David <matei@buoyant.io>
2022-10-18 09:54:45 +0000

    Rm unused config
    Signed-off-by: Matei David <matei@buoyant.io>

--- fish/config.fish
+++ fish/config.fish
@@ -14,29 +14,3 @@ funcsave fish_greeting
 alias gst='git status'
 alias k='kubectl'
 alias kgp='kubectl get pods'
-
-# VSCode
-function code
-  set location "$PWD/$argv"
-  open -n -b "com.microsoft.VSCode" --args $location
-end
-
-# ghcup-env
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
-test -f /Users/madavid/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin /Users/madavid/.ghcup/bin $PATH
-
-# Go env
-set -x GOPATH "/Users/madavid/go"
-set -x GOBIN "/Users/madavid/go/bin"
-
-# Ripgrep&FZF
-# RTFM man fzf
-set -x FZF_DEFAULT_COMMAND "rg --files --hidden"
-
-# Artylab
-set -x ARTY_LAB "public-docker-virtual.artylab.expedia.biz"
-
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/madavid/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/madavid/Downloads/google-cloud-sdk/path.fish.inc'; end
-set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
