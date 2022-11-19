02b26e5fad899497b43a31d3839b93b1428555c5
Matei David <matei@buoyant.io>
2022-11-19 16:30:29 +0000

    Add updates
    Signed-off-by: Matei David <matei@buoyant.io>

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,8 +7,17 @@ require('mappings').core_bindings()
 --vim.g.tokyonight_style = "night"
 --cmd[[colorscheme tokyonight]] -- set color theme
 --require('nightfox').load('nightfox')
-cmd([[colorscheme catppuccin]])
+--cmd([[colorscheme catppuccin]])
 
 cmd([[command! Bufs :buffers]])
-
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, { pattern = { '*.lua' }, command = 'Format' })
+vim.api.nvim_create_autocmd(
+    { 'BufWritePre' },
+    { pattern = { '*.rs', '*.go' }, callback = vim.lsp.buf.formatting_sync }
+)
+--cmd([[autocmd BufWritePre *.lua Format]])
+--cmd([[autocmd BufWritePre *.go vim.lsp.buf.formatting_sync()]])
+--cmd([[autocmd BufWritePre *.rust vim.lsp.buf.formatting_sync()]]) lol, do I
+--even write Rust? Filetype "RUST"
+-- autocmd BufWritePre *.go lua vim.lsp.buf.formatting()
 require('lang-conf')
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -184,6 +184,11 @@ return require('packer').startup(function(use)
     use({
         'catppuccin/nvim',
         as = 'catppuccin',
+        config = function()
+          vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+		  require("catppuccin").setup()
+		  vim.api.nvim_command "colorscheme catppuccin"
+        end,
     })
 
     use({
