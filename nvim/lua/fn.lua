5b47d3949bfe96fcbe9179f7ce17c301d2a7e346
Matei David <matei@buoyant.io>
2022-11-21 11:33:14 +0000

    Disable lua-line
    Signed-off-by: Matei David <matei@buoyant.io>

--- nvim/lua/fn.lua
+++ nvim/lua/fn.lua
@@ -2,13 +2,13 @@
 -- human readable format.
 -- e.g P(package.loaded)
 P = function(v)
-	print(vim.inspect(v))
-	return v
+  print(vim.inspect(v))
+  return v
 end
 
 -- R(<name>: string) is a helper function that will force a lua package to be
 -- reloaded.
-M = function(name)
-	package.loaded[name] = nil
-	require(name)
+R = function(name)
+  package.loaded[name] = nil
+  require(name)
 end
--- nvim/lua/lang-conf.lua
+++ nvim/lua/lang-conf.lua
@@ -1,36 +1,37 @@
 local lspconfig = require('lspconfig')
 
 -- //
--- // Diagnostics 
+-- // Diagnostics
 -- //
 -- Disable virtual_text, using lsp_lines
 vim.diagnostic.config({
-    virtual_text = false,
-    virtual_lines = { only_current_line = true },
+  virtual_text = false,
+  --virtual_lines = { only_current_line = true },
+  virtual_lines = false,
 })
 
-
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+vim.lsp.handlers['textDocument/publishDiagnostics'] =
+  vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
     -- Enable underline, use default values
     underline = true,
     signs = {
-        enable = true,
-        priority = 20,
+      enable = true,
+      priority = 20,
     },
     -- Disable a feature
     update_in_insert = false,
-})
+  })
 
-local on_attach = function(client, bufnr)
-    require('mappings').lsp_bindings(bufnr)
-    local buf_set_option = function(...)
-        vim.api.nvim_buf_set_option(bufnr, ...)
-    end
+local on_attach = function(_client, bufnr)
+  require('mappings').lsp_bindings(bufnr)
+  local buf_set_option = function(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
 
-    buf_set_option('textwidth', 80)
+  buf_set_option('textwidth', 80)
 
-    -- Remove 't'? added 'q', 'j'
-    buf_set_option('formatoptions', 'c' .. 'r' .. 'q' .. 'b' .. 'j')
+  -- Remove 't'? added 'q', 'j'
+  buf_set_option('formatoptions', 'c' .. 'r' .. 'q' .. 'b' .. 'j')
 end
 
 -- //
@@ -39,54 +40,54 @@ end
 local luasnip = require('luasnip')
 local cmp = require('cmp')
 cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-            luasnip.lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        end,
-    },
-    mapping = {
-        ['<C-p>'] = cmp.mapping.select_next_item(),
-        ['<C-n>'] = cmp.mapping.select_prev_item(),
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ['<C-e>'] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-        }),
-        ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end,
-        ['<S-Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end,
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    },
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' }, -- For luasnip users.
-    }, {
-        { name = 'buffer' },
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_next_item(),
+    ['<C-n>'] = cmp.mapping.select_prev_item(),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
     }),
-    completion = {
-        autocomplete = false,
-    },
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end,
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }, -- For luasnip users.
+  }, {
+    { name = 'buffer' },
+  }),
+  completion = {
+    autocomplete = false,
+  },
 })
 
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
@@ -96,50 +97,50 @@ local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- //
 -- Go
 lspconfig.gopls.setup({
-    cmd = { 'gopls', 'serve' },
-    on_attach = on_attach,
-    capabilities = lsp_capabilities,
-    init_options = {
-        usePlaceholders = true,
-        completeUnimported = true,
-    },
+  cmd = { 'gopls', 'serve' },
+  on_attach = on_attach,
+  capabilities = lsp_capabilities,
+  init_options = {
+    usePlaceholders = true,
+    completeUnimported = true,
+  },
 })
 
 -- Rust
 lspconfig.rust_analyzer.setup({
-    on_attach = on_attach,
-    capabilities = lsp_capabilities,
-    commands = {
-        Format = {
-            function()
-                require('stylua-nvim').format_file()
-            end,
-        },
+  on_attach = on_attach,
+  capabilities = lsp_capabilities,
+  commands = {
+    Format = {
+      function()
+        require('stylua-nvim').format_file()
+      end,
     },
+  },
 })
 
 -- Lua
 lspconfig.sumneko_lua.setup({
-    on_attach = on_attach,
-    capabilities = lsp_capabilities,
-    commands = {
-        Format = {
-            function()
-                require('stylua-nvim').format_file()
-            end,
-        },
+  on_attach = on_attach,
+  capabilities = lsp_capabilities,
+  commands = {
+    Format = {
+      function()
+        require('stylua-nvim').format_file()
+      end,
     },
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' },
-            },
-        },
+  },
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' },
+      },
     },
+  },
 })
 --require('rust-tools').setup({})
 
 lspconfig.bashls.setup({
-    on_attach = on_attach,
-    capabilities = lsp_capabilities,
+  on_attach = on_attach,
+  capabilities = lsp_capabilities,
 })
