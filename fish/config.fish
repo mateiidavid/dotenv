cf91d5c8e2943fdab16311de026f7192f1232285
Matei David <matei@buoyant.io>
2022-12-06 11:39:49 +0000

    Mv new fish config over
    Signed-off-by: Matei David <matei@buoyant.io>

--- fish/config.fish
+++ fish/config.fish
@@ -1,42 +1,23 @@
-# My Fishy conf
-# Starship prompt
-starship init fish | source
-
 # Greetings my good sir
 function fish_greeting
    printf '\n\n'
    fortune 
 end
 
-funcsave fish_greeting
-
 # Aliases
 alias gst='git status'
 alias k='kubectl'
 alias kgp='kubectl get pods'
-
-# VSCode
-function code
-  set location "$PWD/$argv"
-  open -n -b "com.microsoft.VSCode" --args $location
-end
-
-# ghcup-env
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
-test -f /Users/madavid/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin /Users/madavid/.ghcup/bin $PATH
-
-# Go env
-set -x GOPATH "/Users/madavid/go"
-set -x GOBIN "/Users/madavid/go/bin"
+alias tmx='tmux attach || tmux new'
+alias ls='exa --icons'
+alias la='exa -la --icons --colour-scale'
+alias ll='exa -abghHli --icons'
 
 # Ripgrep&FZF
 # RTFM man fzf
 set -x FZF_DEFAULT_COMMAND "rg --files --hidden"
 
-# Artylab
-set -x ARTY_LAB "public-docker-virtual.artylab.expedia.biz"
+# My Fishy conf
+# Starship prompt
+starship init fish | source
 
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/madavid/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/madavid/Downloads/google-cloud-sdk/path.fish.inc'; end
-set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
